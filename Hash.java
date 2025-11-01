68bcba9625cd6fb0d3455fef72974eef0ef759a1
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-02 07:56:37 +0800

    hash sample

--- Hash.java
+++
@@ -1,8 +0,0 @@
-public class Hash {
-    public static void main(String[] args) {
-        ///add your hash implementation here
-        System.out.println("Hash implementation placeholder");
-
-        
-    }
-}
