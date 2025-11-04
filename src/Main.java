dbc0ce908fbe6a211d86e909e3b8896403bc4b51
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-05 05:23:14 +0800

    s

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a sample  Data Structure and Algorithms project.");
-        System.out.println("Place your code here.");
+        System.out.println("Place your code here .");
 
     }
 
