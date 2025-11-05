cf4157ea0107b6a14fa4dc48cd1eb18728e5afaf
Josiah44123 <163369246+Josiah44123@users.noreply.github.com>
2025-11-05 12:12:30 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a sample  Data Structure and Algorithms project.");
         System.out.println("Place your code here .");
+        int num = scan.nextInt();
+
+        
 
     }
 
